dd58c3a0b9c146f24d1c14612efdbf623b18bf8b
Katrin <kateyka1410@gmail.com>
2024-09-04 20:38:53 +0300

    .

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -42,7 +42,10 @@ const slice = createSlice({
         state.loading = true;
         state.error = null;
       })
-      
+         .addCase(deleteContact.fulfilled, (state, action) => {
+        state.loading = false;
+        state.items = state.items.filter(item => item.id !== action.payload.id);
+      })
       .addCase(deleteContact.rejected, (state) => {
         state.loading = false;
         state.error = true;
